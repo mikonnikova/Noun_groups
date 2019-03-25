2291bf22236564f1e2d777f00e7130aac15df6e7
mikonnikova <42408633+mikonnikova@users.noreply.github.com>
2019-03-25 13:23:35 +0300

    Update chunker.py

--- chunker.py
+++ chunker.py
@@ -7,6 +7,7 @@ import h5py
 from keras.models import Model
 from keras.layers import Input, LSTM, Dropout, Dense, TimeDistributed, Embedding, concatenate
 from keras.preprocessing.sequence import pad_sequences
+from keras.optimizers import Adam
 
 from keras_contrib.layers import CRF
 from keras_contrib.losses import crf_loss
@@ -44,28 +45,28 @@ def train_chunker(words_file, pos_file, answers_file, model_file, embedding_matr
 
     # pos embedding
     word_embedding = Embedding(embedding_matrix.shape[0], embedding_matrix.shape[1], weights=[embedding_matrix],
-                               trainable=False)(words_sequence)
-    post_word_embedding = Dense(25, activation='tanh')(word_embedding)
-    pos_embedding = Embedding(18, 10)(pos_sequence)
+                               trainable=False, mask_zero=True)(words_sequence)
+    post_word_embedding = Dense(15, activation='tanh')(word_embedding)
+    pos_embedding = Embedding(18, 15, mask_zero=True)(pos_sequence)
 
     # internal dropout
-    in_dp_words = Dropout(0.5)(post_word_embedding)
-    in_dp_pos = Dropout(0.5)(pos_embedding)
+    in_dp_words = Dropout(0.3)(post_word_embedding)
+    in_dp_pos = Dropout(0.3)(pos_embedding)
 
     # internal merge
     merged1 = concatenate([in_dp_words, in_dp_pos])
 
     # forwards LSTM
-    forwards = LSTM(units=50, return_sequences=True)(merged1)
+    forwards = LSTM(units=15, return_sequences=True)(merged1)
     # backwards LSTM
-    backwards = LSTM(go_backwards=True, units=50, return_sequences=True)(merged1)
+    backwards = LSTM(go_backwards=True, units=15, return_sequences=True)(merged1)
 
     # concatenate the outputs of the 2 LSTMs
     merged2 = concatenate([forwards, backwards])
-    after_dp = Dropout(0.5)(merged2)
+    after_dp = Dropout(0.3)(merged2)
 
     # output layer
-    timed = TimeDistributed(Dense(1, activation='relu'))(after_dp)
+    timed = TimeDistributed(Dense(10, activation='relu'))(after_dp)
     output = CRF(3)(timed)
 
     model = Model(inputs=[words_sequence, pos_sequence], outputs=output)
@@ -74,17 +75,46 @@ def train_chunker(words_file, pos_file, answers_file, model_file, embedding_matr
     model.compile(loss=crf_loss, optimizer='adam', metrics=[crf_accuracy])
 
     batch_size = 16
+    prev_loss = 1
+    rounds_without_improvement = 0
+
     for _ in range(30):
+        new_loss = 0
 
         # padding and training on batch
-        for i in range(num_examples // batch_size):
+        for i in range(num_examples // batch_size - 1):
             words = pad_sequences(words_train[i * batch_size:(i + 1) * batch_size], padding='post')
             pos = pad_sequences(pos_train[i * batch_size:(i + 1) * batch_size], padding='post')
             answer = pad_sequences(answers[i * batch_size:(i + 1) * batch_size], padding='post')
 
-            model.train_on_batch([words, pos], answer)
+            new_loss += model.train_on_batch([words, pos], answer)[0]
 
-        # TODO compare quality for early stopping
+        i = num_examples // batch_size
+        print(new_loss)
+
+        # last batch
+        if num_examples % batch_size > 0:
+            words = pad_sequences(words_train[i * batch_size:], padding='post')
+            pos = pad_sequences(pos_train[i * batch_size:], padding='post')
+            answer = pad_sequences(answers[i * batch_size:], padding='post')
+
+            new_loss += model.train_on_batch([words, pos], answer)[0]
+            new_loss /= (i+1)
+
+        else:
+            new_loss /= i
+
+        print(new_loss)
+        # loss improvement
+        if abs(prev_loss - new_loss) <= 1e-3:
+            rounds_without_improvement += 1
+        else:
+            rounds_without_improvement = 0
+        prev_loss = new_loss
+
+        # early stopping
+        if rounds_without_improvement >= 2:
+            break
 
         # shuffle data
         words_train, pos_train, answers = shuffle(words_train, pos_train, answers)
